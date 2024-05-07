fef8cb7a1af585cb1152716d431edf9e38637be0
hyf112 <yh706@georgetown.edu>
2024-05-06 23:58:57 -0400

    Delete unnecessary packages

--- chatdf/src/utils/utils.py
+++ chatdf/src/utils/utils.py
@@ -1,35 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
 import os
-import google.generativeai as genai
 from openai import OpenAI
 import polars as pl
 import streamlit as st
 import duckdb
-
-
-def gemini_setup():
-    api_key = os.getenv('API_KEY')
-    genai.configure(api_key=api_key)
-
-    try:
-        for m in genai.list_models():
-            if 'generateContent' in m.supported_generation_methods:
-                print(m.name)
-    except Exception:
-        raise Exception("Please check your API key")
-    
-def openai_setup():
-    client = OpenAI(api_key=os.environ.get('Openai'))
-    return client
-
-
-def pl_loadLazy(data_path, response_message):
-    NYCTLC = pl.read_parquet(data_path).lazy()
-    with pl.SQLContext(register_globals=True) as NYCTLC:
-        res = NYCTLC.execute(response_message+";").collect()
-
-    return res
 
 
 def execute_sql_query(data_path, query, df_name):
